f483ecdaa0a1e4ea3f796e90e0dfaee5f7a70891
Sukrutiii <suk613939@gmail.com>
2022-09-17 14:13:31 +0530

    deploying app

--- backend/server.js
+++ backend/server.js
@@ -12,27 +12,24 @@ connectDB();
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-       res.send("API is running..");
-  });
 
 app.use('/api/users', userRoutes);
 app.use('/api/notes', noteRoutes);
 
 // --------------------------deployment------------------------------
-// __dirname = path.resolve();
+__dirname = path.resolve();
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "/frontend/build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running..");
-//   });
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 // --------------------------deployment------------------------------
 
 
