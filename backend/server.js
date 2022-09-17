ea31224e16c43c5e18723f42a87e22c37c95561a
Sukrutiii <suk613939@gmail.com>
2022-09-17 16:11:51 +0530

    make it better

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("API is running..");
-});
+// app.get("/", (req, res) => {
+//   res.send("API is running..");
+// });
 
 app.use('/api/users', userRoutes);
 app.use('/api/notes', noteRoutes);
@@ -26,9 +26,9 @@ app.use('/api/notes', noteRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
+  // app.get("*", (req, res) =>
+  //   res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  // );
 
 }
 
