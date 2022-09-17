41b05bd6b87194223fe89b0d899f67615415f4bb
Sukrutiii <suk613939@gmail.com>
2022-09-17 15:41:40 +0530

    notesapp

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,15 @@ const connectDB = require("./config/db");
 const userRoutes = require('./routes/userRoutes');
 const noteRoutes = require('./routes/noteRoutes');
 const { notFound, errorHandler }=require("./middleware/errorMiddleware");
+const cors = require("cors");
+const path = require("path");
+
 
 dotenv.config();
 connectDB();
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("API is running..");
@@ -16,6 +20,17 @@ app.get("/", (req, res) => {
 
 app.use('/api/users', userRoutes);
 app.use('/api/notes', noteRoutes);
+
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+
+}
 
 
 app.use(notFound);
