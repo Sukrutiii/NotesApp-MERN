2be9f71b1d5852c623621c87b9893dd569dfa8d6
Sukrutiii <suk613939@gmail.com>
2022-09-17 13:59:25 +0530

    changes added

--- backend/server.js
+++ backend/server.js
@@ -6,28 +6,33 @@ const noteRoutes = require('./routes/noteRoutes');
 const { notFound, errorHandler }=require("./middleware/errorMiddleware");
 const path = require("path");
 
-const app = express();
+
 dotenv.config();
 connectDB();
+const app = express();
 app.use(express.json());
+
+app.get("/", (req, res) => {
+       res.send("API is running..");
+  });
 
 app.use('/api/users', userRoutes);
 app.use('/api/notes', noteRoutes);
 
 // --------------------------deployment------------------------------
-__dirname = path.resolve();
+// __dirname = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running..");
+//   });
+// }
 // --------------------------deployment------------------------------
 
 
