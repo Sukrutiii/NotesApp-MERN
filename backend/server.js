740f3c853696a54515f7bc081f950c586d538403
Sukrutiii <suk613939@gmail.com>
2022-09-17 14:22:32 +0530

    added changes in server.js

--- backend/server.js
+++ backend/server.js
@@ -4,33 +4,18 @@ const connectDB = require("./config/db");
 const userRoutes = require('./routes/userRoutes');
 const noteRoutes = require('./routes/noteRoutes');
 const { notFound, errorHandler }=require("./middleware/errorMiddleware");
-const path = require("path");
-
 
 dotenv.config();
 connectDB();
 const app = express();
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("API is running..");
+});
 
 app.use('/api/users', userRoutes);
 app.use('/api/notes', noteRoutes);
-
-// --------------------------deployment------------------------------
-__dirname = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
-// --------------------------deployment------------------------------
 
 
 app.use(notFound);
